6f5e83eb535001b893ed6f07bc1df235f0ec3274
mateus <mateusf.alves@outlook.com>
2025-08-26 21:20:12 -0300

    Criando um objeto

---
+++ index.js
@@ -0,0 +1,18 @@
+var alunos = [
+    {
+        RA: 12347,
+        nome: 'Mateus',
+        cursos: ['Desenvolvimento Web', 'Engenharia de Software', 'UX']
+    },
+
+    {
+        RA: 14728,
+        nome: 'Maria',
+        cursos: ['Anatomia', 'Cistologia', 'Química']
+    },
+    {
+        RA: 15673,
+        nome: 'Antonia',
+        cursos: ['História Geral', 'Antropologia', 'Museologia']
+    }
+];
